6a11857430c6096caa7c98af61d71b5af5bbd6f8
Zime Pascal <zimepascalt@gmail.com>
2024-11-19 14:30:06 +0100

    Add file connection.h

---
+++ connection.h
@@ -0,0 +1,23 @@
+#ifndef CONNEXION_H
+#define CONNEXION_H
+
+#include "QSqlDatabase"
+#include "QSqlQuery"
+
+static bool Connexion()
+{
+    QSqlDatabase Connexion = QSqlDatabase::addDatabase("QMYSQL");// select SGBD you use
+    Connexion.setHostName("127.0.0.1"); // input the address of localhost or hostname
+    Connexion.setUserName("root");// input user name
+    Connexion.setDatabaseName("pharmastock"); // select the database name
+    Connexion.setPort(3306); // choose the port
+    if(Connexion.open())
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+}
+
+#endif // CONNEXION_H
